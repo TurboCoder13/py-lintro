3a5a2dbee2c7812edd6994306008955ce934fd22
TurboCoder13 <turbocoder13@gmail.com>
2026-02-21 09:54:42 +0100

    fix(tools): export ResizeMethod from ascii_resizer package

--- tools/ascii_resizer/__init__.py
+++ tools/ascii_resizer/__init__.py
@@ -1,6 +1,6 @@
 """ASCII art resizing and standardization tool."""
 
-from .resizer import AsciiResizer
+from .resizer import AsciiResizer, ResizeMethod
 
-__all__ = ["AsciiResizer"]
+__all__ = ["AsciiResizer", "ResizeMethod"]
 __version__ = "0.1.0"
